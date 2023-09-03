bf7e91176f4f48a707973794326dfd4605a46f23
mdshakhawat-H <mdshakhawathossain441@hotmail.com>
2023-09-03 00:25:21 -0600

    Banner Added

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$('.bnr_slider').slick();
+
+'use strict';
+
+const type = document.querySelector('.type-text');
+
+let listTexts = ['Divine School , Jadurchar'];
+
+let index = -1;
+let current = 0;
+
+function typeTextFunc() {
+    if (current === listTexts.length) {
+        current = 0;
+    }
+    let typeText = setInterval(() => {
+        if (index == listTexts[current].length - 1) {
+            clearInterval(typeText);
+            clearTextFunc();
+        } else {
+            index++;
+            type.textContent += listTexts[current][index];
+        }
+    }, 150);
+};
+
+function clearTextFunc() {
+    let clearText = setInterval(() => {
+        if (type.textContent.length == 0) {
+            clearInterval(clearText);
+            index = -1;
+            current++
+            typeTextFunc();
+        } else {
+            type.textContent = type.textContent.slice(0, -1)
+        }
+    }, 150);
+};
+
+typeTextFunc();
