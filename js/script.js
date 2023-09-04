ab2929d90cb952781d8836ebf32acb40195f0185
mdshakhawat-H <mdshakhawathossain441@hotmail.com>
2023-09-03 23:47:27 -0600

    Section Added

--- js/script.js
+++ js/script.js
@@ -60,3 +60,42 @@ arr.map((item)=>{
 
 
 // ================ WELCOME PART ENDED ===============
+// ================ RECENT PART START ===============
+$('.recent_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    prevArrow: '<i class="fa-solid fa-caret-left slick_left"></i>',
+    nextArrow: '<i class="fa-solid fa-caret-right slick_right"></i>',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 991.98,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767.98,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+// ================ RECENT PART ENDED ===============
